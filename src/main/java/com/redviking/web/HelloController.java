a8e18df3a12f4d8f8fffc0f7d5615287ef2d1dc9
zfall <fall.zoltan@gmail.com>
2013-06-13 20:51:47 +0200

    adding logback

--- src/main/java/com/redviking/web/HelloController.java
+++ src/main/java/com/redviking/web/HelloController.java
@@ -6,14 +6,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 public class HelloController implements Controller {
 
-    protected final Log logger = LogFactory.getLog(getClass());
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
